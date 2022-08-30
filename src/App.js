6aa39a7b379129bc05afffe08a643d3592076619
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-08-30 20:47:29 +0300

    initial exercise setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
 function App() {
-  return (
-    <div className="App">
-      Hello React!
-    </div>
-  );
+  return <h2>reminder project setup</h2>;
 }
 
 export default App;
