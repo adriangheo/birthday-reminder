956acd5cb2d8e9f7a7ff9736cc5f2df07f91ba3b
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-08-30 20:47:55 +0300

    final version of code

--- src/data.js
+++ src/data.js
@@ -1,37 +1,38 @@
 export default [
-  {
-    id: 1,
-    name: 'Bertie Yates',
-    age: 29,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
-  },
-  {
-    id: 2,
-    name: 'Hester Hogan',
-    age: 32,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
-  },
-  {
-    id: 3,
-    name: 'Larry Little',
-    age: 36,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
-  },
-  {
-    id: 4,
-    name: 'Sean Walsh',
-    age: 34,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
-  },
-  {
-    id: 5,
-    name: 'Lola Gardner',
-    age: 29,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-  },
-];
+    {
+      id: 1,
+      name: 'Joanne Doe',
+      age: 26,
+      image:
+        'https://i.imgur.com/0NCTyhU.jpg',
+    },
+    {
+      id: 2,
+      name: 'Homer Guy',
+      age: 28,
+      image:
+        'https://i.imgur.com/KdgPIh9.jpg',
+    },
+    {
+      id: 3,
+      name: 'Paul Weber',
+      age: 41,
+      image:
+        'https://i.imgur.com/1vKUAHA.jpg',
+    },
+    {
+      id: 4,
+      name: 'Lexie Holloway',
+      age: 25,
+      image:
+        'https://i.imgur.com/UFKg0K0.jpg',
+    },
+    {
+      id: 5,
+      name: 'Maxine Broughtonr',
+      age: 34,
+      image:
+        'https://i.imgur.com/6ERnLJf.jpg',
+    },
+  ];
+  
